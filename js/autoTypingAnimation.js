7c053b029caa00b8bb222ad8270d54aa37cded54
purwo <purwosantososkom@gmail.com>
2025-09-21 01:27:17 +0700

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, Saya Purwo Santoso</span> Lulusan Universitas Gunadarma Depok tahun 2020 Jurusan Sistem Komputer, Pengalaman saya di dunia web developer kurang lebih 5 tahun. Selama 5 tahun, saya telah membimbing siswa untuk menguasai pengembangan web kompleks, membuka jalan bagi mereka untuk karier di industri teknologi. Di samping itu, saya juga menyediakan layanan pengembangan web profesional sebagai Freelancer, memastikan saya tetap relevan dan terampil. "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
